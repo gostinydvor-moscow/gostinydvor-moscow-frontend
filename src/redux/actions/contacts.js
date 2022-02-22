8558aafe8f0b6e887d306dd79150ff5c124fb3e4
gostinydvor-moscow <98814548+gostinydvor-moscow@users.noreply.github.com>
2022-02-22 12:12:21 +0300

    Update contacts.js

--- src/redux/actions/contacts.js
+++ src/redux/actions/contacts.js
@@ -1,33 +1,39 @@
-import axios from 'axios'
+// import axios from 'axios'
+import $api from '../../http/';
 
 export const sendFormContact = (data) => (dispatch) => {
-	const newData = new FormData()
-
-	newData.append("values", JSON.stringify({
-		"CONTACT_SECOND_NAME": [
-			data.question
-		],
-		"DEAL_TITLE": [
-			data.task
-		],
-		"CONTACT_NAME": [
-			data.name
-		],
-		"CONTACT_LAST_NAME": [
-			data.contact
-		],
-		"CONTACT_EMAIL": [
-			data.email
-		]
-	}))
-
-	newData.append("id", 10)
-	newData.append("sec", "ivu02n")
-
-	axios.post("https://b24-7a6nyl.bitrix24.ru/bitrix/services/main/ajax.php?action=crm.site.form.fill", newData).then(() => {
+	$api.post("/contacts", data).then(() => {
 		dispatch(setContactsIsSend(true));
 		dispatch(setContactsQuestion(""));
 	})
+
+	// const newData = new FormData()
+	
+	// newData.append("values", JSON.stringify({
+	// 	"CONTACT_SECOND_NAME": [
+	// 		data.question
+	// 	],
+	// 	"DEAL_TITLE": [
+	// 		data.task
+	// 	],
+	// 	"CONTACT_NAME": [
+	// 		data.name
+	// 	],
+	// 	"CONTACT_LAST_NAME": [
+	// 		data.contact
+	// 	],
+	// 	"CONTACT_EMAIL": [
+	// 		data.email
+	// 	]
+	// }))
+
+	// newData.append("id", 10)
+	// newData.append("sec", "ivu02n")
+
+	// axios.post("https://b24-7a6nyl.bitrix24.ru/bitrix/services/main/ajax.php?action=crm.site.form.fill", newData).then(() => {
+	// 	dispatch(setContactsIsSend(true));
+	// 	dispatch(setContactsQuestion(""));
+	// })
 }
 
 export const setContactsQuestion = (question) => ({
